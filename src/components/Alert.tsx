303ec18f2e2db13bc66f178149b61914218d9d2e
Jun Ukemori <jukemori@gmail.com>
2023-06-14 15:05:45 +0900

    Alert

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,3 +1,13 @@
-function Alert() {}
+import { ReactNode } from "react";
 
-export default Alert;
+interface Props {
+  children: ReactNode;
+}
+
+const Alert = ({ children }: Props) => {
+  return (
+    <div className="alert alert-primary ">{children}</div>
+  )
+}
+
+export default Alert
