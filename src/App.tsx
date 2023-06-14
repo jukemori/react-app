fe38cfb10a5e2c233fb5dd6bd552df7eef74cd6d
Jun Ukemori <jukemori@gmail.com>
2023-06-14 15:43:35 +0900

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
-
+  const [alertVisisble, setAlertVisibility] = useState(false);
   return (
     <div>
-      <Button color='primary'onClick={() => console.log('Clicked')}>My Button</Button>
+      { alertVisisble && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>}
+      <Button color='primary'onClick={() => setAlertVisibility(true)}>My Button</Button>
     </div>
   );
 }
